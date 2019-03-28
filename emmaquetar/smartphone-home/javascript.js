078b2963d6aee279a159d612695525025769ce8a
Darya Ampilogova Volkodav <d.ampilogova@innovation.es>
2019-03-28 14:27:11 +0100

    updated files

--- emmaquetar/smartphone-home/javascript.js
+++ emmaquetar/smartphone-home/javascript.js
@@ -1,10 +1,39 @@
-function closeOrOpen() {
-    var fisrtButton = document.getElementById
-    var secondButton = document.getElementById("#second-button");
+// Les següents funcions s'executaràn un pic s'hagui carregat la pàgina, gràcies al .ready()
 
-    if ($("#second-button").css("transform", "rotate(-90deg)")) {
-        secondButton.onclick = $("#second-button").css("transform", "rotate(0deg)");
-    }
+$(document).ready(function() {
+
+    // En clicar damunt el botó de la fletxa, s'executaràn el següent
+    $(".button-arrow").click(function() {
+
+        // Si a la hora de clicar la fletxa està en direcció esquerra...
+        if(parseInt($(this).css("transform").split(",")[1])==0) {
+
+            // Li direm a través del CSS, que es col·loqui en direcció cap a baix
+            $(this).css("transform", "rotate(-90deg)");
+
+            // A continuació amagarem el contingut del missatge
+            $(this).parent().parent().children(".message-content").hide();
+        }
+
+        // Si en clicar la fletxa està en direcció cap a baix...
+        else {
+
+            // La girarem en direcció cap a la esquerra
+            $(this).css("transform", "rotate(0deg)");
+
+            // Farem visible el missatge
+            $(this).parent().parent().children(".message-content").show();
+        }
+    });
+});
 
 
-}
+$(document).ready(function() {
+
+    // En clicar damunt el botó del DELETE, s'executarà el següent
+    $(".delete-message").click(function() {
+
+        // Indiquem borrar el missatge sencer a través de la funció rmeove()
+        $(this).parent().parent().parent().remove();
+    });
+});
