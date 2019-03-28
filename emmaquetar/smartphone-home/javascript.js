d4cf171ae070bc54adb96a24718a1ad348e8a8dd
Darya Ampilogova Volkodav <d.ampilogova@innovation.es>
2019-03-28 11:02:05 +0100

    add project

---
+++ emmaquetar/smartphone-home/javascript.js
@@ -0,0 +1,10 @@
+function closeOrOpen() {
+    var fisrtButton = document.getElementById
+    var secondButton = document.getElementById("#second-button");
+
+    if ($("#second-button").css("transform", "rotate(-90deg)")) {
+        secondButton.onclick = $("#second-button").css("transform", "rotate(0deg)");
+    }
+
+
+}
